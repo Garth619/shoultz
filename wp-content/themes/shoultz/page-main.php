23f27512f2495c6d4c421aaa4f30e8f23085fb83
Garrett Cullen <garrettwcullen@gmail.com>
2016-08-12 09:17:56 -0700

    push

--- wp-content/themes/shoultz/page-main.php
+++ wp-content/themes/shoultz/page-main.php
@@ -373,7 +373,21 @@ get_header(); ?>
 			
 		</section><!-- reasons -->
 		
-		
+		<section class="respect">
+			
+			<span class="large_header">RESPECTED BY THE LEGAL COMMUNITY</span><!-- large_header -->
+			
+			<div class="respect_content">
+				
+				<p><span class="letter">L</span>orem ipsum dolor sit amet, consec consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodiem sis consequat. Duis aute irure dolor in reprereprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem. ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat. voluptatem.</p>
+				
+			</div><!-- respect_content -->
+			
+			<img class="respect_img" src="<?php bloginfo('template_directory');?>/images/shoultz_content_results-01.svg"/>
+			
+		</section><!-- respect -->
 		
 		
 		
