b15ecd9ad02b330bdbf91e3e31b28e87d017d66f
garrett cullen <info@garrettcullen.com>
2016-08-21 21:30:05 -0700

    push

---
+++ wp-content/themes/shoultz/page-testimonials.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Template Name: Testimonials
+
+ */
+
+get_header(); ?>
+
+
+<?php include('banner.php');?>
+
+
+<div id="main">
+	
+	
+	<img class="inner_testimonial_img" src="<?php bloginfo('template_directory');?>/images/shoultz_test_testimonials.svg"/>
+	
+	<span class="large_header about_us_header">What Our CLients are saying</span><!-- large_header -->
+	
+	
+	<div id="content">
+	
+		
+		<div id="container">
+			
+			<div class="single_test_wrapper">
+					
+					<div class="stars_wrapper">
+						
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+					
+					</div><!-- stars_wrapper -->
+					
+					<span class="test_title">AN ATTORNEY THAT TRULY CARES ABOUT HIS&nbsp;CLIENTS</span>
+					
+					<span class="test_content">Steve has handled my personal injury case with such compassion. His attention to detail is second to none. My life will never truly be the same due to getting injured but I thank the Lord I've had such a caring and methodical attorney in my corner. Thanks for everything you've done&nbsp;Steve.</span>
+					
+					
+					<span class="test_name">Debbie - avvo</span>
+					
+				</div><!-- single_test_wrapper -->
+				
+				
+				<div class="single_test_wrapper">
+					
+					<div class="stars_wrapper">
+						
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+					
+					</div><!-- stars_wrapper -->
+					
+					<span class="test_title">AN ATTORNEY THAT TRULY CARES ABOUT HIS&nbsp;CLIENTS</span>
+					
+					<span class="test_content">Steve has handled my personal injury case with such compassion. His attention to detail is second to none. My life will never truly be the same due to getting injured but I thank the Lord I've had such a caring and methodical attorney in my corner. Thanks for everything you've done&nbsp;Steve.</span>
+					
+					
+					<span class="test_name">Debbie - avvo</span>
+					
+				</div><!-- single_test_wrapper -->
+				
+				
+				<div class="single_test_wrapper">
+					
+					<div class="stars_wrapper">
+						
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+						<img class="stars" src="<?php bloginfo('template_directory');?>/images/shoultz_test_star.svg"/>
+					
+					</div><!-- stars_wrapper -->
+					
+					<span class="test_title">AN ATTORNEY THAT TRULY CARES ABOUT HIS&nbsp;CLIENTS</span>
+					
+					<span class="test_content">Steve has handled my personal injury case with such compassion. His attention to detail is second to none. My life will never truly be the same due to getting injured but I thank the Lord I've had such a caring and methodical attorney in my corner. Thanks for everything you've done&nbsp;Steve.</span>
+					
+					
+					<span class="test_name">Debbie - avvo</span>
+					
+				</div><!-- single_test_wrapper -->
+			
+			
+				     
+		</div><!-- container -->
+
+
+	
+	
+	</div><!-- content -->
+	
+		
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
