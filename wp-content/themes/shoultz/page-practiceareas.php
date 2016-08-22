59d416564308c9d84768fd21a151df43dc6d6a9c
garrett cullen <info@garrettcullen.com>
2016-08-21 20:59:27 -0700

    push

---
+++ wp-content/themes/shoultz/page-practiceareas.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template Name: Practice Areas
+
+ */
+
+get_header(); ?>
+
+
+<?php include('banner.php');?>
+
+
+<div id="main">
+	
+	
+	<img class="dedicated_img" src="<?php bloginfo('template_directory');?>/images/desktop/internal_pa_dedicated.svg"/>
+	
+	<span class="large_header about_us_header">Practice Areas</span><!-- large_header -->
+	
+	
+	<div id="content">
+	
+		
+		<div id="container">
+			
+			
+			<ul class="practice_area">
+				<li><a href="">Practice Area</a></li>
+				<li><a href="">Practice Area</a></li>
+				<li><a href="">Practice Area</a></li>
+				<li><a href="">Practice Area</a></li>
+				<li><a href="">Practice Area</a></li>
+				<li><a href="">Practice Area</a></li>
+				<li><a href="">Practice Area</a></li>
+			</ul><!-- practice_area -->
+			
+			
+				     
+		</div><!-- container -->
+
+
+	
+	
+	</div><!-- content -->
+	
+		
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
