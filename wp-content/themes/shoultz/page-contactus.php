4bf1d2b0f840efedcbdab62404657923d9c2e0a1
Garrett Cullen <garrettwcullen@gmail.com>
2016-08-22 09:42:39 -0700

    push

---
+++ wp-content/themes/shoultz/page-contactus.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template Name: Contact Us
+
+ */
+
+get_header(); ?>
+
+
+<?php include('banner.php');?>
+
+
+<div id="main">
+	
+	
+	<img class="inner_testimonial_img" src="<?php bloginfo('template_directory');?>/images/desktop/internal_contact.svg"/>
+	
+	<div class="contact_wrapper">
+	
+		<span class="large_header about_us_header">Contact</span><!-- large_header -->
+		
+		<span class="title">Address</span><!-- title -->
+		
+		<span class="contact_content">900 Jackson Street, Suite 340<br/>Dallas, Texas 75202</span>
+		
+		<span class="title">Phone</span><!-- title -->
+		
+		<span class="contact_content">214.960.1551</span>
+		
+		<span class="title">Email</span><!-- title -->
+		
+		<span class="contact_content">email@email.com</span>
+	
+	</div><!-- contact_wrapper -->
+	
+	<div class="map">
+		
+
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3354.541530131915!2d-96.80551344914085!3d32.77789769115421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e991766ad75e1%3A0x9ba9a4de594a454b!2sLaw+Office+of+Stephen+W.+Shoultz!5e0!3m2!1sen!2sus!4v1471883636983" width="100%" height="289" frameborder="0" style="border:0" allowfullscreen></iframe>
+		
+	
+		<a href="https://www.google.com/maps/place/Law+Office+of+Stephen+W.+Shoultz/@32.7778977,-96.8055134,17z/data=!4m5!3m4!1s0x864e991766ad75e1:0x9ba9a4de594a454b!8m2!3d32.7778932!4d-96.8033194?hl=en-US" target="_blank" class="get_directions_bar">Get Directions</a><!-- get_directions_bar -->
+	
+	
+	</div><!-- map -->
+		
+		
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
