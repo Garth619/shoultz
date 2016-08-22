12e58364679146eaa70b8008a98014ce3d59320d
garrett cullen <info@garrettcullen.com>
2016-08-21 19:23:07 -0700

    push

---
+++ wp-content/themes/shoultz/banner.php
@@ -0,0 +1,20 @@
+<div class="inner_banner">
+	
+	<div class="inner_banner_wrapper">
+	
+	<a href="">
+		<div class="play"></div>
+	</a>
+	
+	<div class="inner_banner_content">
+			
+		<span class="main_banner_header">Over <span class="thirty_five">35 years</span> of<br/>legal experience</span>
+				
+		<a class="mybutton inner_pages" href="">CLICK HERE FOR A FREE CONSULTATION</a>
+				
+				
+		</div><!-- inner_banner_content -->
+			
+	</div><!-- inner_banner_wrapper -->
+	
+</div><!-- inner_banner -->
---
+++ wp-content/themes/shoultz/page-firmoverview.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Template Name: Firm Overview
+
+ */
+
+get_header(); ?>
+
+
+<?php include('banner.php');?>
+
+
+<div id="main">
+	
+	
+	<img class="about_img" src="<?php bloginfo('template_directory');?>/images/desktop/internal_firmoverview_about.svg"/>
+	
+	<span class="large_header about_us_header">PERSONALIZED ATTENTION & INDIVIDUALIZED APPROACH</span><!-- large_header -->
+	
+	
+	<div id="content">
+	
+		
+		<div id="container">
+			
+			
+				     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem. ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et</p>
+
+<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem. ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+</div><!-- container -->
+
+<span class="pull_quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore dolor sit amet, consectetur adipi dolor sit amet, consectetur adipi amet</span><!-- pull_quote -->
+	
+	
+	</div><!-- content -->
+	
+	<section class="logos">
+			
+			<div class="logo_slider">
+				
+				<div class="single_logo">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/shoultz_awards_1.png"/>
+					
+				</div><!-- single_logo -->
+				
+				<div class="single_logo">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/shoultz_awards_2.png"/>
+					
+				</div><!-- single_logo -->
+				
+				<div class="single_logo">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/shoultz_awards_1.png"/>
+					
+				</div><!-- single_logo -->
+				
+				<div class="single_logo">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/shoultz_awards_2.png"/>
+					
+				</div><!-- single_logo -->
+				
+			</div><!-- logo_slider -->
+			
+		</section><!-- logos -->
+	
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
