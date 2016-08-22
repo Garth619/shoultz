e625a98126304b82c17da890374ee196f1ea4fbd
garrett cullen <info@garrettcullen.com>
2016-08-21 22:04:15 -0700

    push

---
+++ wp-content/themes/shoultz/page-videocenter.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Template Name: Video Center
+
+ */
+
+get_header(); ?>
+
+
+<?php include('banner.php');?>
+
+
+<div id="main">
+	
+	
+	<img class="inner_testimonial_img" src="<?php bloginfo('template_directory');?>/images/desktop/internal_video_reliable.svg"/>
+	
+	<span class="large_header about_us_header">Video Center</span><!-- large_header -->
+	
+	
+	<div id="content">
+	
+		
+		<div id="container">
+			
+			
+			
+			
+			<div class="single_video">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/desktop/temp.jpg"/><!-- video_temp -->
+				
+				<a class="video_title" href="">TITLE OF VIDEO GOES HERE</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/desktop/temp.jpg"/><!-- video_temp -->
+				
+				<a class="video_title" href="">TITLE OF VIDEO GOES HERE</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/desktop/temp.jpg"/><!-- video_temp -->
+				
+				<a class="video_title" href="">TITLE OF VIDEO GOES HERE</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/desktop/temp.jpg"/><!-- video_temp -->
+				
+				<a class="video_title" href="">TITLE OF VIDEO GOES HERE</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/desktop/temp.jpg"/><!-- video_temp -->
+				
+				<a class="video_title" href="">TITLE OF VIDEO GOES HERE</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/desktop/temp.jpg"/><!-- video_temp -->
+				
+				<a class="video_title" href="">TITLE OF VIDEO GOES HERE</a>
+				
+			</div><!-- single_video -->
+			
+			
+			
+				     
+		</div><!-- container -->
+
+
+	
+	
+	</div><!-- content -->
+	
+		
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
