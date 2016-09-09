14b744ac5b42d1b9e6b875f3edd349479ffecc5b
Garrett Cullen <garrettwcullen@gmail.com>
2016-09-09 10:47:31 -0700

    push

--- wp-content/themes/shoultz/functions.php
+++ wp-content/themes/shoultz/functions.php
@@ -607,7 +607,6 @@ function my_jquery_enqueue() {
 }
 
 
-/*
 
 add_filter( 'gform_confirmation_anchor', '__return_true' );
 
@@ -615,7 +614,7 @@ add_filter( 'gform_confirmation_anchor', '__return_true' );
 
 function load_my_styles_scripts() {
      // Load my stylesheet
-     wp_enqueue_style( 'style', get_stylesheet_uri(), '', 3, 'screen' ); 
+     wp_enqueue_style( 'style', get_stylesheet_uri(), '', 1, 'screen' ); 
      
  
 		 wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom-min.js',
@@ -628,7 +627,6 @@ function load_my_styles_scripts() {
  
  
  add_action( 'wp_enqueue_scripts', 'load_my_styles_scripts', 20 );
-*/
 
 
 
